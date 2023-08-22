f36e337c6b375f9579c4c2567c83749ece3e86f1
EnzoFatecTavares <enzo.tavares@fatec.sp.gov.br>
2023-08-21 22:17:11 -0300

    Os 4 Testes

--- contas.test.js
+++ contas.test.js
@@ -1,6 +1,21 @@
 const contas = require('./contas');
 
-test("Soma 2 + 5 esperando 7 como resultado", ()=> {
-    expect(contas.soma(2,5)).toBe(7)
+test("Adição 2 + 5 esperando 7 como resultado", ()=> {
+    expect(contas.multiplicacao(2,5)).toBe(7)
+    
+})
+
+test("Multiplicação 2 * 5 esperando 10 como resultado", ()=> {
+    expect(contas.multiplicacao(2,5)).toBe(10)
+    
+})
+
+test("Divisão 5 / 2 esperando 2.5 como resultado", ()=> {
+    expect(contas.multiplicacao(5,2)).toBe(2.5)
+    
+})
+
+test("Subtração 5 - 2 esperando 3 como resultado", ()=> {
+    expect(contas.multiplicacao(5,2)).toBe(3)
     
 })
